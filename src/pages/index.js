c92ec7e0071c617b2f0be25639b62b999bf25ad1
lkennxyz <liam.kennedy1408@gmail.com>
2019-03-01 19:43:20 +0000

    made the site

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,48 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import twitter from "../images/twitter-icon.svg"
+import github from "../images/github-icon.svg"
+import linkedIn from "../images/linkedin-icon.svg"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <SEO title="Home" keywords={[`dev`, `javascript`]} />
+    <div style={{ maxWidth: `300px`, margin: `auto`, borderRadius: `50%` }}>
+      <Image className="mainPic"/>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className="links">
+      <div className="me">
+        <a href="https://blog.kenn.dev">
+          blog
+        </a>
+        <a href="https://overkill.kenn.dev">
+          Project Overkill
+        </a>
+      </div>
+      <div className="social">
+        <a href="https://twitter.com/lkennxyz">
+          <span className="icon">
+            <img src={twitter} alt="twitter"/>
+          </span>
+          <p>twitter</p>
+        </a>
+        <a href="https://github.com/lkennxyz">
+          <span className="icon">
+            <img src={github} alt="github"/>
+          </span>
+          <p>github</p>
+        </a>
+        <a href="https://www.linkedin.com/in/lkennxyz">
+          <span className="icon">
+            <img src={linkedIn} alt="linkedin"/>
+          </span>
+          <p>linkedIn</p>
+        </a>
+      </div>
+    </div>
   </Layout>
 )
 
